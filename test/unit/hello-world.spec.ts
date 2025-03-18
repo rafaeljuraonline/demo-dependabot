ecd457ceca8d75b6b363161617aa92a3b497bf2d
Rafael Rodriguez <rafael.rodriguez@jura-online.de>
2025-03-18 17:47:45 +0100

    update

--- test/unit/hello-world.spec.ts
+++ test/unit/hello-world.spec.ts
@@ -1,8 +1,9 @@
 
-
 describe('Hello World', () => {
     it('should return "Hello, World!"', () => {
+        const localTime = new Date().toLocaleString();
         console.log('Hello, World!');
+        console.log(`Local Time: ${localTime}`);
         expect('Hello, World!').toBe('Hello, World!');
     });
 });

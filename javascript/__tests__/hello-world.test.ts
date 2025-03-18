d47ae8aec35742ffa2f62f898fe2ce8ddf6d75fe
Rafael Rodriguez <rafael.rodriguez@jura-online.de>
2025-03-18 14:48:36 +0100

    added test workflow

---
+++ javascript/__tests__/hello-world.test.ts
@@ -0,0 +1,8 @@
+
+
+describe('Hello World', () => {
+    it('should return "Hello, World!"', () => {
+        console.log('Hello, World!');
+        expect('Hello, World!').toBe('Hello, World!');
+    });
+});
